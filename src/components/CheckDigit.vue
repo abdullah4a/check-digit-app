248a6c7d21ee1c7fc0cbcdf966134c35bfc77314
abdullah4a <abdullahkhan4a@gmail.com>
2022-10-27 23:53:44 +0500

    Solved Valid ISBN Check

---
+++ src/components/CheckDigit.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="hello">
+    <h1>Check Valid ISBN-13</h1>
+    <div class="check-digit-inputs" id="check-digit-inputs">
+      <input
+        v-model="isbnInput"
+        type="text"
+        name="check-digit-input"
+        id="check-digit-input"
+        @focus="clearMessages"
+      />
+      <input
+        type="button"
+        value="Check ISBN"
+        @click="checkISBN"
+        id="check-digit-button"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckDigit",
+  data() {
+    return {
+      isbnInput: "",
+    };
+  },
+  methods: {
+    checkISBN() {
+      const dataArray = this.isbnInput.split("");
+      if (dataArray.length === 12) {
+        this.findLast(dataArray);
+      } else if (dataArray.length === 13) {
+        this.checkValidISBN13(dataArray);
+      } else {
+        this.printIt("Not a valid ISBN-13 number", "error");
+      }
+    },
+    findLast(data) {
+      let sum = 0;
+      for (let index = 0; index < data.length; index++) {
+        index % 2 === 0 ? (sum += data[index] * 1) : (sum += data[index] * 3);
+      }
+      const mod = sum % 10;
+      data.push(10 - mod);
+      this.checkValidISBN13(data);
+    },
+    printIt(value, className) {
+      const checkDigitInputs = document.getElementById("check-digit-inputs");
+      const newNodeP = document.createElement("p");
+      const nodePText = document.createTextNode(value);
+      if(className==="error"){
+        newNodeP.style.color="red";
+      }else if(className==="success"){
+        newNodeP.style.color="green";
+      }
+      newNodeP.appendChild(nodePText);
+      checkDigitInputs.appendChild(newNodeP);
+    },
+    checkValidISBN13(data) {
+      let sum = 0;
+      for (let index = 0; index < data.length; index++) {
+        index % 2 === 0 ? (sum += data[index] * 1) : (sum += data[index] * 3);
+      }
+      const joinnedISBN = data.join("");
+      sum % 10 === 0
+        ? this.printIt(joinnedISBN + " is a Valid ISBN number", "success")
+        : this.printIt("Not a valid ISBN", "error");
+    },
+    clearMessages() {
+      const checkDigitInputs = document.getElementById("check-digit-inputs");
+      if(checkDigitInputs.childNodes.length>2){
+        checkDigitInputs.removeChild(checkDigitInputs.childNodes[2])
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.check-digit-inputs {
+  width: 30%;
+  margin: auto;
+}
+#check-digit-input {
+  font-size: 20px;
+  padding: auto;
+  margin: auto;
+  border-radius: 0 0;
+}
+#check-digit-button {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 10px 0;
+}
+</style>
